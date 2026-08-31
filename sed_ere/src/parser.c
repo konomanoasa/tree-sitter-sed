e0ce883778ba0183c452cb5c9338e66829f04049
konomanoasa <238482287+konomanoasa@users.noreply.github.com>
2026-08-31 13:57:39 +0900

    chore: release 0.5.0

--- sed_ere/src/parser.c
+++ sed_ere/src/parser.c
@@ -55789,7 +55789,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_sed_ere(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 4,
+      .minor_version = 5,
       .patch_version = 0,
     },
   };
