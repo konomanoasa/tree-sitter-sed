b572ad801253ca0c649b56c29a80fb991c7428ce
konomanoasa <238482287+konomanoasa@users.noreply.github.com>
2026-09-02 14:42:42 +0900

    chore: release 0.7.0

--- src/parser.c
+++ src/parser.c
@@ -57941,7 +57941,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_sed(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 6,
+      .minor_version = 7,
       .patch_version = 0,
     },
   };
